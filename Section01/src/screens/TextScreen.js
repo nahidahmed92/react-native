dfa33913355ab279fa1e04ed7c199e6df83bf8c6
Nahid Ahmed <npahmed92@gmail.com>
2024-12-29 23:33:56 -0500

    Section 06.67 - Password Input Validation

--- Section01/src/screens/TextScreen.js
+++ Section01/src/screens/TextScreen.js
@@ -3,6 +3,7 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const TextScreen = () => {
   const [name, setName] = useState('');
+  const [password, SetPassword] = useState('');
 
   return (
     <View>
@@ -17,6 +18,22 @@ const TextScreen = () => {
         value={name}
       />
       <Text>Hi! My name is {name}</Text>
+
+      <Text>Enter Password:</Text>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={(newPass) => {
+          SetPassword(newPass);
+        }}
+        style={styles.textInput}
+        value={password}
+      />
+      {password.length > 5 ? (
+        <Text style={{ color: 'green' }}>Password requirement meant</Text>
+      ) : (
+        <Text style={{ color: 'red' }}>Password must be longer than 5 characters</Text>
+      )}
     </View>
   );
 };
